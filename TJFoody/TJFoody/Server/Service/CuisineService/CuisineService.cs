5ccfe02103d11324f7c1a2a86ddb168de2c2e611
climoiiin <1309824959@qq.com>
2023-06-09 23:47:18 +0800

    cuisine相关

---
+++ TJFoody/TJFoody/Server/Service/CuisineService/CuisineService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TJFoody.Server.Models;
+
+namespace TJFoody.Server.Service.CuisineService
+{
+    public class CuisineService : ICuisineService
+    {
+        private readonly infoContext _context;
+        public CuisineService(infoContext infoContext)
+        {
+            _context = infoContext;
+        }
+
+        public async Task<ServiceResponse<EntityEntry<Cuisine>>> AddCuisineAsync(Cuisine cuisine)
+        {
+            var response = new ServiceResponse<EntityEntry<Cuisine>>();
+            
+            try
+            {
+                await _context.Cuisines.AddAsync(cuisine);
+                await _context.SaveChangesAsync();
+
+                response.Message = "成功";
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = "添加菜品时发生错误：" + ex.Message;
+            }
+
+            return response;
+        }
+
+        public async Task<ServiceResponse<List<Cuisine>>> GetCuisineBySellerIDAsync(int id)
+        {
+            var response = new ServiceResponse<List<Cuisine>>
+            {
+                Data = await _context.Cuisines.Where(c => c.SellerId == id).ToListAsync()
+            };
+
+            return response;
+        }
+
+        public async Task<ServiceResponse<Cuisine>> GetCuisineByIDAsync(int id)
+        {
+            var response = new ServiceResponse<Cuisine>();
+
+            try
+            {
+                response.Data = await _context.Cuisines.FindAsync(id);
+                response.Message = "成功找到了对应菜品";
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = "在查找菜品时发生了错误：" + ex.Message;
+            }
+            return response;
+        }
+
+    }
+}
---
+++ TJFoody/TJFoody/Server/Service/CuisineService/ICuisineService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace TJFoody.Server.Service.CuisineService
+{
+    public interface ICuisineService
+    {
+        Task<ServiceResponse<EntityEntry<Cuisine>>> AddCuisineAsync(Cuisine cuisine);
+        Task<ServiceResponse<Cuisine>> GetCuisineByIDAsync(int id);
+
+        Task<ServiceResponse<List<Cuisine>>> GetCuisineBySellerIDAsync(int id);
+    }
+}
