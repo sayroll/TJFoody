d895921fbc5730b1cf28b98625175fd88db8bf29
sayroll <83714631+sayroll@users.noreply.github.com>
2023-06-10 14:45:01 +0800

    登陆注册

--- TJFoody/TJFoody/Client/Program.cs
+++ TJFoody/TJFoody/Client/Program.cs
@@ -2,6 +2,8 @@ global using TJFoody.Shared;
 global using System.Net.Http.Json;
 global using TJFoody.Client.Services.SellerService;
 global using TJFoody.Client.Services.UserService;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TJFoody.Client;
@@ -9,12 +11,16 @@ using TJFoody.Client.Services.CuisineService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after"); 
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ISellerService, SellerService>();
 builder.Services.AddScoped<ICuisineService, CuisineService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider,CustomAuthStateProvider>();
 
 builder.Services.AddBootstrapBlazor();
 await builder.Build().RunAsync();
